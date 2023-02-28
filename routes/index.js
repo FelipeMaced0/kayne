348626427031d97f793f84fa98e7f903062803d7
Felipe Macedo <fmacedo746@gmail.com>
2023-02-28 14:38:49 -0300

    Fixing the routes to public files

--- routes/index.js
+++ routes/index.js
@@ -16,32 +16,32 @@ router.get('/about', function(req, res, next){
 
 /* IMAGES */
 router.get('/public/images/kanye-stare.gif', function(req, res, next) {
-  res.sendFile('/home/iky/Documents/java-script/kayne/public/images/kanye-stare.gif');
+  res.sendFile('../public/images/kanye-stare.gif');
 });
 
 router.get('/public/images/kayne-microphone.jpeg', function(req, res, next) {
-  res.sendFile('/home/iky/Documents/java-script/kayne/public/images/kayne-microphone.jpeg');
+  res.sendFile('../public/images/kayne-microphone.jpeg');
 });
 /* IMAGES */
 
 /* JS AND CSS */
 router.get('/public/javascripts/controller.js', function(req, res, next) {
-  res.sendFile('/home/iky/Documents/java-script/kayne/public/javascripts/controller.js');
+  res.sendFile('../public/javascripts/controller.js');
 });
 
 router.get('/public/javascripts/navbarController.js', function(req, res, next) {
-  res.sendFile('/home/iky/Documents/java-script/kayne/public/javascripts/navbarController.js');
+  res.sendFile('../public/javascripts/navbarController.js');
 });
 /* JS AND CSS */
 
 
 /* AUDIO */
 router.get('/public/audio/look-at-this-dude.mp3', function(req, res, next) {
-  res.sendFile('/home/iky/Documents/java-script/kayne/public/audio/look-at-this-dude.mp3');
+  res.sendFile('../public/audio/look-at-this-dude.mp3');
 });
 
 router.get('/public/audio/mission-failed.mp3', function(req, res, next) {
-  res.sendFile('/home/iky/Documents/java-script/kayne/public/audio/mission-failed.mp3');
+  res.sendFile('../public/audio/mission-failed.mp3');
 });
 /* AUDIO */
 
